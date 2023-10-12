fcc3ab89d2878cc0138c8951f3509a3e473b5a03
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-11 21:34:53 -0400

    added manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add babel to webpack (plugins).
-
 module.exports = () => {
   return {
     mode: "development",
@@ -24,8 +21,25 @@ module.exports = () => {
         title: "Webpack Plugin",
       }),
       new InjectManifest({
-        swSrc: "./src-sw.js",
+        // make sure pointing to right file //
+        swSrc: "src-sw.js",
         swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Jate",
+        short_name: "Jate",
+        description: "Text Editor",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
@@ -42,6 +56,10 @@ module.exports = () => {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
