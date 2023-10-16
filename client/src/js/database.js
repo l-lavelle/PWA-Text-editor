ecf501e411f7879f78ff696b974b8ae64894fadd
Lauren <lauren.m.lavelle@wmich.edu>
2023-10-16 11:32:27 -0400

    added error catching

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,25 +14,31 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ content: content, id: 1 });
-  const result = await request;
-  console.log("Data saved to database", result);
-  console.error("putDb not implemented");
+  try {
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readwrite");
+    const store = tx.objectStore("jate");
+    const request = store.put({ content: content, id: 1 });
+    const result = await request;
+    console.log("Data saved to database", result);
+  } catch (err) {
+    console.error("putDb not implemented");
+  }
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const request = store.getAll();
-  const result = await request;
-  console.log("result.value", result);
-  console.error("getDb not implemented");
-  return result.value;
+  try {
+    const jateDB = await openDB("jate", 1);
+    const tx = jateDB.transaction("jate", "readonly");
+    const store = tx.objectStore("jate");
+    const request = store.getAll();
+    const result = await request;
+    console.log("result.value", result);
+    return result.value;
+  } catch (err) {
+    console.error("getDb not implemented");
+  }
 };
 
 initdb();
